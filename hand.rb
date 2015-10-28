f426797b0cc33dcfd7af900953b9078b95a01275
matugm <matugm@gmail.com>
2015-10-28 20:00:25 +0100

    Add some comments

--- hand.rb
+++ hand.rb
@@ -8,14 +8,17 @@ class Hand
     @cards    = hand.split.map { |card| Card.new(card) }
   end
 
+  # Returns an array of raw card valuues
   def numbers
     @cards.map(&:number)
   end
 
+  # Returns an array of absolute card values
   def values
     @cards.map(&:value)
   end
 
+  # Returns the original input string
   def inspect
     @original
   end
